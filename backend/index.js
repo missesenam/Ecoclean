fe4d9d6a853a4d16eccb03830990e692702fff6f
missesenam <moneyesenam@gmail.com>
2025-06-04 11:51:16 +0100

    my commit

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-require("dotenv").config();
+require("dotenv").config({ path: __dirname + "/.env" });
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 // routers
